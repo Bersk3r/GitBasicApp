c4687c6c9f551b841ff185896a47a123c40a3abe
Bersk3r <kiscwook@gmail.com>
2023-05-17 16:36:14 +0900

    ul list item added!

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // index.html을 위한 JS
+var ulElement = document.getElementById("myUl");
+var li = ulElement.append('li');
+ulElement.append('li');
+console.log('추가된 로그');
+console.log('소스트리에서 commit을 amend하기');
